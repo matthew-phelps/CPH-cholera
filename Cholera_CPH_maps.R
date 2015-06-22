687abbf20cad839da29a4b3363a8359dbe6ca808
Matthew Phelps <mdp553@hotmail.com>
2015-06-22 14:33:22 +0200

    Cleaned code and using New QuarterID numbers - not using the one's from the original excel doc

--- Cholera_CPH_maps.R
+++ Cholera_CPH_maps.R
@@ -7,10 +7,11 @@
 rm(list = ls())
 graphics.off()
 mac <- "/Users/Matthew/Google Drive/Cholera Denmark/CPH/Data"
-pc <- 'C:\\Users\\wrz741\\Google Drev\\Copenhagen\\DK Cholera\\CPH\\Data\\'
+pc <- 'C:\\Users\\wrz741\\Google Drive\\Copenhagen\\DK Cholera\\CPH\\Data\\'
 
 setwd(pc)
-Sys.setenv(JAVA_HOME='C:/Program Files/Java/jre1.8.0_31') 
+Sys.setenv(JAVA_HOME='C:/Program Files/Java/jre1.8.0_45') 
+
 library (xlsx) # reading excel files
 library(foreign)
 library(sp)
@@ -20,27 +21,17 @@ library(maptools)
 library(RColorBrewer)
 library(ggmap)
 
+load("Rdata\\quarter_eng.Rdata")
+quarter.sheet <- rename(quarter, replace = c("sick.total.week" = "I"))
 
-quarter.sheet <- read.xlsx2(file = "quarter.xlsx",
-                            sheetIndex = 1, colClasses = c("character", rep("numeric", 8)))
-quarter.sheet <- rename(quarter.sheet, replace = c("sick.total.week" = "I"))
-
-# reshape data
-quarter.wide <- reshape(quarter.sheet, timevar = "week.id", idvar = c("quarter", "quarterID", "pop1855"), direction ="wide")
-#rm(quarter.sheet)
 
 # shapefile
-quarter.shp <- readOGR(dsn = "GIS", layer = "CPH_Quarters", stringsAsFactors = F)
+quarter.shp <- readOGR(dsn = "GIS", layer = "CPH_Quarters_eng", stringsAsFactors = F)
 plot(quarter.shp)
 quarter.shp@data$id <- as.numeric(quarter.shp@data$id)
 quarter.shp@data
 
-## Merging data to .shp file. This is not needed to R work, but used to write a
-## .shp file that can be read into a GIS
-#quarter.shp@data <- merge(quarter.shp@data, quarter.wide, by.x = "id", by.y = "quarterID")
-#quarter.shp@data$Quarter <- NULL
-#writeOGR(quarter.shp, dsn = "GIS", layer = "CHP_Quarters1", driver = "ESRI Shapefile", overwrite_layer = T)
-#rm(quarter.wide)
+
 
 
 # Get spatial data into a form that ggplot2 can handle
@@ -48,6 +39,8 @@ quarter.shp@data
 quarter.df <- as.data.frame(quarter.shp)
 quarter.fortified <-fortify(quarter.shp, region = "id")
 # quarter.lines <- join (quarter.fortified, quarter.df, by = "id")
+
+quarter.fortified$id <- as.numeric(quarter.fortified$id)
 
 mapdf <- merge(quarter.fortified, quarter.sheet, by.x = "id", by.y = "quarterID", all = T )
 mapdf <- mapdf[ order(mapdf$order),]
@@ -73,7 +66,7 @@ cph +
                                     fill = (cum.sick/pop1855)*100),
                   color = "black") +
      #coord_equal(ratio = 0) +
-     scale_fill_gradientn(colours = brewer.pal(9, "Reds")) +
+     scale_fill_gradientn(name = "Cumulative Infections \nper 100 people", colours = brewer.pal(9, "Reds")) +
           
      theme(axis.title.x = element_blank(), # remove x,y, label
            axis.title.y = element_blank(),
@@ -85,14 +78,14 @@ cph +
            panel.grid.major = element_blank(),
            panel.background = element_blank()
      ) +
-     ggtitle("Infectious per week")
+     ggtitle("Normalized Cumulative Infections ")
 
 # map matrix showing the No. infectious at weekly intervals---------------------
 
 cph +
      geom_polygon(data = mapdf, aes(x = long, y = lat, group = id,  fill = (I/pop1855)*100),
                   color = "black") +
-     scale_fill_gradientn(colours = brewer.pal(9, "Reds")) +
+     scale_fill_gradientn(name = "Incident Infections \nper 100ppl", colours = brewer.pal(9, "Reds")) +
      facet_wrap(~ week.id)+
      
      theme(axis.title.x = element_blank(), # remove x,y, label
@@ -105,7 +98,7 @@ cph +
            panel.grid.major = element_blank(),
            panel.background = element_blank()
      ) +
-     ggtitle("Infectious per week")
+       ggtitle("Infectious per week per 100 ppl")
 
 
 
