eedfd658c13e38fd6175892e858296ad30025d36
Matthew Phelps <mdp553@hotmail.com>
2015-06-22 13:03:11 +0200

    Changed reshaphe file to match new combined quarters

--- 5_GLM_data_reshape.R
+++ 5_GLM_data_reshape.R
@@ -17,6 +17,7 @@ library(ggplot2)
 library(plyr)
 
 load("Rdata\\quarter_eng.Rdata")
+load("Rdata\\quarter_combined_topography.Rdata")
 # quarter.sheet <- read.xlsx2(file = "quarter.xlsx",
 #                             sheetIndex = 1, 
 #                             colClasses = c("character", rep("numeric", 8)))
@@ -61,11 +62,12 @@ save(quarter.glm, file = "Rdata\\quarter_glm.Rdata")
 
 
 
-# Reshape data - MERGED QUARTERS\ -----------------------------------------
+# Reshape data - NO TOPOGRAPHY -----------------------------------------
 quarter.merged.glm <- quarter.sheet[ order(quarter.sheet$week.id, quarter.sheet$quarterID),] # order by chronology
 quarter.merged.glm$pop1855 <- quarter.merged.glm$dead.total.week <- NULL
 
-quarter.merged.glm$Quarter <- factor(quarter.merged.glm$quarter,levels=c("Christianshavn","Frimands","Kjoebmager","Klaedebo","Noerre","Nyboder","Oester","Rosenborg","Snarens","St. Annae Vester","St. Annae Oester", "Strand","Vester"),labels=c("Christianshavn","Frimands","Kjoebmager","Klaedebo","Noerre","Nyboder","Oester","Rosenborg","Snarens","St.Annae.Vester","St.Annae.Oester", "Strand","Vester"))
+quarter.merged.glm$Quarter <- factor(quarter.merged.glm$quarter,levels=c("Christianshavn","Frimands","Kjoebmager","Klaedebo","Noerre","Nyboder","Oester","Rosenborg","Snarens","St. Annae Vester","St. Annae Oester", "Strand","Vester"),
+                                     labels=c("Christianshavn","Frimands","Kjoebmager","Klaedebo","Noerre","Nyboder","Oester","Rosenborg","Snarens","St.Annae.Vester","St.Annae.Oester", "Strand","Vester"))
 quarter.merged.glm$quarter <- quarter.merged.glm$Quarter
 levels(quarter.merged.glm$quarter) <- list("Christianshavn"="Christianshavn",
                                            "Nyboder"="Nyboder",
@@ -93,7 +95,52 @@ quarter.merged.glm$quarter
 
 # combine date based no name and week
 
-
-
-
 save(quarter.merged.glm, file = "Rdata\\quarter_combined_glm.Rdata")
+
+
+
+
+# Topograhy based quarter merge -------------------------------------------
+
+quarter.sheet <- topo.combined
+quarter.sheet <- rename(quarter.sheet, replace = c("sick.total.week" = "I.t"))
+
+
+quarter.glm <- quarter.sheet[ order(quarter.sheet$week.id, quarter.sheet$quarterID),] # order by chronology
+quarter.glm$pop1855 <- quarter.glm$dead.total.week <- NULL
+
+quarter.glm$Quarter <- factor(quarter.glm$quarter,
+                              levels=c("Christianshavn","Kjoebmager", "Nyboder",
+                                       "Oester", "Rosenborg","St. Annae Oester",
+                                       "St. Annae Vester", "combined.upper", 
+                                       "combined.lower"),
+                              labels=c("Christianshavn","Kjoebmager", "Nyboder","Oester",
+                                       "Rosenborg", "St.Annae.Oester", "St.Annae.Vester",
+                                       "combined.upper", "combined.lower"))
+quarter.glm$quarter <- quarter.glm$Quarter
+levels(quarter.glm$quarter) <- list("Christianshavn"="Christianshavn",
+                                    "Kjoebmager"="Kjoebmager",
+                                    "Nyboder"="Nyboder",
+                                    "Oester"="Oester",
+                                    "Rosenborg"="Rosenborg",
+                                    "St.Annae.Oester"="St.Annae.Oester",
+                                    "St.Annae.Vester"="St.Annae.Vester",
+                                    "combined.upper" = "combined.upper",
+                                    "combined.lower" = "combined.lower")
+# add columns to df to be filled in later
+quarter.week <- split(quarter.glm,quarter.glm$week.id)
+quarter.week <- lapply(1:length(quarter.week),function(w){
+  week.data <- quarter.week[[w]]
+  # browser()
+  if (w==1)
+    sick.total.previous.week <- matrix(0,ncol=9,nrow=9)
+  else
+    sick.total.previous.week <- matrix(quarter.week[[w-1]][,"I.t"],ncol=9,nrow=9,byrow=TRUE)
+  colnames(sick.total.previous.week) <- week.data$quarter
+  cbind(week.data,sick.total.previous.week)
+})
+quarter.glm <- do.call("rbind",quarter.week)
+quarter.glm$quarter
+topo.combined <- quarter.glm
+# fill in columns in df
+save(topo.combined, file = "Rdata\\quarter_topo_combined_glm.Rdata")
