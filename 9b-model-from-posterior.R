d22d6416838eb3ccd576e643060695b75046593a
Matthew Phelps <mdp553@hotmail.com>
2015-11-10 09:32:34 -0500

    City level S and I plots

--- 9b-model-from-posterior.R
+++ 9b-model-from-posterior.R
@@ -17,16 +17,16 @@ library(reshape)
 # LOAD data ---------------------------------------------------------------
 
 load(file = 'data\\Rdata\\model_sim_data.Rdata')
-
+set.seed(123)
 
 
 # MODEL -------------------------------------------------------------------
 
 # For each model run, create 8x8 matrix so each neighborhood pair has beta estimate
 # Value is drawn from one row of MCMC posterior. Sampleing with replacement
-loops <- 3000
+loops <- 30
 I_est_list <- list()
-
+S_it_est_list <- list()
 for (z in 1:loops){
   
   # Use MCMC output from one MCMC draw
@@ -49,26 +49,59 @@ for (z in 1:loops){
     }
   }
   I_est_list[[z]] <- I_it_est
+  S_it_est_list[[z]] <- S_it_est
 }
 
 
 # DATA RESHAPING ----------------------------------------------------------
 
-# Matrix that ggplot can handle
+# Infectious Data for all quarters (city level). Flatten each matrix
+city <- as.data.frame(matrix(data = 0, nrow = 16, ncol = loops))
+city$week_index <- 1:Nsteps
+for (z in 1:loops){
+city[z] <- as.data.frame(colSums(I_est_list[[z]]))
+}
+city_melt <- melt(city, id.vars = 'week_index')
+
+
+# Infectious for single quarter
 I_quarter <-(I_est_list[[1]][1, ])
 for (z in 2:(loops)){
-I_quarter <- rbind(I_quarter, I_est_list[[z]][1, ])
+  I_quarter <- rbind(I_quarter, I_est_list[[z]][1, ])
 }
 I_quarter <- as.data.frame(t(I_quarter))
-I_quarter$week_index <- 1:16
+I_quarter$week_index <- 1:Nsteps
 I_quarter_melt <- melt(I_quarter, id.vars = 'week_index')
+
+
+# Susceptible city-wide
+city_S <- as.data.frame(matrix(data = 0, nrow = 16, ncol = loops))
+city_S$week_index <- 1:Nsteps
+for (z in 1:loops){
+  city_S[z] <- as.data.frame(colSums(S_it_est_list[[z]]))
+}
+city_S_melt <- melt(city_S, id.vars = 'week_index')
 
 
 
 # PLOTS -------------------------------------------------------------------
 
+# Quartery Infectious (Christianshavn)
 ggplot(data = I_quarter_melt, aes(x = week_index, y = value, group = variable)) +
-  geom_line(color = 'darkred', alpha = 0.05)
+  geom_line(color = 'darkred', alpha = 0.05) +
+  ggtitle('Christianshavn simulated\n n = 3000')
+
+
+# City level Infectious
+ggplot(data = city_melt, aes(x = week_index, y = value, group = variable)) +
+  geom_line(color = 'darkgreen', alpha = 0.05) +
+  ggtitle('City level simulated\n n = 3000')
+
+
+# City level Infectious
+ggplot(data = city_S_melt, aes(x = week_index, y = value, group = variable)) +
+  geom_line(color = 'darkblue', alpha = 0.05) +
+  ggtitle('City level Susceptible\n n = 3000')
 
 # Plot last model run
 # plot(S_it_est[1, ], type = 'l', col = 'darkred', lwd = 2)
