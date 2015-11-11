b79e1d7c91fd277abeeb468b2c6944ece56ba289
Matthew Phelps <mdp553@hotmail.com>
2015-11-11 10:38:31 -0500

    Simplify data prep for JAGS
    Move data prep step into separate file

--- 8b_model_1_2.R
+++ 8b_model_1_2.R
@@ -23,43 +23,7 @@ options(mc.cores = (parallel::detectCores()-1 ))
 
 # COMBINED quarters -------------------------------------------------------
 rm(list = ls())
-load(file = "data\\Rdata\\quarter_combined.Rdata")
-
-
-### Prepare data to send to Stan
-Nsteps <- 16
-quarterID <- as.numeric(combined$quarterID)
-n <- as.numeric(length(quarterID))
-Nquarter <- length(table(quarterID))
-q_names <-as.data.frame(unique(combined$quarter))
-
-S_it <- matrix(0, Nquarter, Nsteps)
-I_it <- matrix(0, Nquarter, Nsteps)
-N_i <- matrix(0, Nquarter, Nsteps)
-for (i in 1:Nquarter){
-  for( t in 1:Nsteps){
-    S_it[i, t] <- (combined$S[which(combined$quarterID==i)])[t]
-    I_it[i, t] <- (combined$sick.total.week[which(combined$quarterID==i)])[t]
-    N_i[i, t] <- (combined$pop1855[which(combined$quarterID==i)])[t]
-  }
-}
-row.names(I_it) <- q_names[, 1]
-
-# 
-# frac_suseptible_it <- matrix(0, nrow = Nquarter, ncol = Nsteps)
-# for (i in 1:Nquarter){
-#   for (t in 1:Nsteps){
-#     frac_suseptible_it[i, t] <- S_ti[i, t] / N_i[i, t]
-#   }
-# }
-
-
-
-dataList <- list(Nquarter=Nquarter,
-                 S_it = S_it,
-                 N_i = N_i,
-                 I_it=I_it,
-                 Nsteps=Nsteps)
+load(file = "Data_3.Rdata")
 
 
 
---
+++ Data-3-perpare-JAGS.R
@@ -0,0 +1,51 @@
+# Author: Matthew Phelps
+#Desc: JAGS model of CPH 1853. Currently uses combined quarters 
+# Dependicies: Data 1, Data 2, 5_GLM_data_reshape
+
+
+
+# Intro -------------------------------------------------------------------
+
+graphics.off()
+mac <- "/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH"
+pc <- "C:\\Users\\wrz741\\Google Drive\\Copenhagen\\DK Cholera\\CPH\\data\\Rdata"
+setwd(pc)
+rm(list = ls())
+library(plyr)
+
+
+# COMBINED quarters -------------------------------------------------------
+rm(list = ls())
+load(file = "quarter_combined.Rdata")
+
+
+### Prepare data to send to Stan
+Nsteps <- 16
+quarterID <- as.numeric(combined$quarterID)
+n <- as.numeric(length(quarterID))
+Nquarter <- length(table(quarterID))
+q_names <-as.data.frame(unique(combined$quarter))
+
+S_it <- matrix(0, Nquarter, Nsteps)
+I_it <- matrix(0, Nquarter, Nsteps)
+N_i <- matrix(0, Nquarter, Nsteps)
+for (i in 1:Nquarter){
+  for( t in 1:Nsteps){
+    S_it[i, t] <- (combined$S[which(combined$quarterID==i)])[t]
+    I_it[i, t] <- (combined$sick.total.week[which(combined$quarterID==i)])[t]
+    N_i[i, t] <- (combined$pop1855[which(combined$quarterID==i)])[t]
+  }
+}
+row.names(I_it) <- q_names[, 1]
+
+
+dataList <- list(Nquarter=Nquarter,
+                 S_it = S_it,
+                 N_i = N_i,
+                 I_it=I_it,
+                 Nsteps=Nsteps)
+
+
+# SAVE --------------------------------------------------------------------
+
+save(list = ls(), file = 'Data_3.Rdata')
