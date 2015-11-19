78a653cae90c31a20da72407f519ecb23cca673b
Matthew Phelps <mdp553@hotmail.com>
2015-11-19 16:47:32 -0500

    Model 0_3 fit by poor convergence

--- model_0_3___1-JAGS_run.R
+++ model_0_3___1-JAGS_run.R
@@ -50,12 +50,12 @@ jags <- jags.model('Rcodes\\stan_model_0_3.stan',
 # JAGS model 0.1 - quarter phi's --------------------------------------------------------------------
 
 model_0_3_jags <- run.jags(model = 'Rcodes\\stan_model_0_3.stan', method = 'parallel',
-                           monitor = c('beta', 'phi', 'lambda'),
+                           monitor = c('beta', 'phi'),
                            data = dataList,
-                           n.chains = 3,
+                           n.chains = 4,
                            adapt = 1000,
                            burnin = 10000,
-                           sample = 20000,
+                           sample = 60000,
                            thin = 3,
                            plots = T)
 
@@ -76,26 +76,44 @@ print(model_0_3_jags)
 # phi1 <- model_0_3_coda[, 1]
 # gelman.diag(phi1)
 
-model_0_1_ggs <- ggs(model_0_3_coda)
+model_0_3_ggs <- ggs(model_0_3_coda)
 
 
 
-trace_beta <- ggs_traceplot(model_0_1_ggs, family = 'beta', simplify = .3)
-density_beta <- ggs_density(model_0_1_ggs, family = 'beta')
-running_beta <- ggs_running(model_0_1_ggs, family = 'beta')
+trace_beta <- ggs_traceplot(model_0_3_ggs, family = 'beta', simplify = .3)
+density_beta <- ggs_density(model_0_3_ggs, family = 'beta')
+running_beta <- ggs_running(model_0_3_ggs, family = 'beta')
 
 
-ggsave(trace_beta, filename = "Output\\MCMC\\trace_beta.jpg")
-ggsave(density_beta, filename = 'Output\\MCMC\\density_beta.jpg')
-ggsave(running_beta, filename = 'Output\\MCMC\\running_beta.png')
+ggsave(trace_beta, filename = "Output\\MCMC\\trace_beta_0_3.jpg")
+ggsave(density_beta, filename = 'Output\\MCMC\\density_beta_0_3.jpg')
+ggsave(running_beta, filename = 'Output\\MCMC\\running_beta_0_3.png')
 
 
-trace_phi <- ggs_traceplot(model_0_1_ggs, family = 'phi', simplify = .3)
-density_phi <- ggs_density(model_0_1_ggs, family = 'phi')
-running_phi <- ggs_running(model_0_1_ggs, family = 'phi')
+trace_phi <- ggs_traceplot(model_0_3_ggs, family = 'phi', simplify = .3)
+density_phi <- ggs_density(model_0_3_ggs, family = 'phi')
+running_phi <- ggs_running(model_0_3_ggs, family = 'phi')
 
 
-ggsave(trace_phi, filename = "Output\\MCMC\\trace_phi.jpg")
-ggsave(density_phi, filename = 'Output\\MCMC\\density_phi.jpg')
-ggsave(running_phi, filename = 'Output\\MCMC\\running_phi.png')
+ggsave(trace_phi, filename = "Output\\MCMC\\trace_phi_0_3.jpg")
+ggsave(density_phi, filename = 'Output\\MCMC\\density_phi_0_3.jpg')
+ggsave(running_phi, filename = 'Output\\MCMC\\running_phi_0_3.png')
 
+
+
+
+# # Get beta PE into human readible matrix --------------------------------
+
+model_0_3_out <-as.data.frame(print(model_0_3_jags))
+nrow(model_0_3_out)
+beta_summary_0_3 <- model_0_3_out[1, ]
+phi_summary_0_3 <- model_0_3_out[2, ]
+
+
+
+# SAVE --------------------------------------------------------------------
+
+save(model_0_3_jags, file = 'data\\Rdata\\model_0_3_jags.Rdata')
+
+save(beta_summary_0_3, file = 'data\\Rdata\\beta_summary_0_3.Rdata')
+save(phi_summary_0_3, file = 'data\\Rdata\\phi_summary_0_3.Rdata')
