d51a4baea7b6adbc6d1525177a5ee069e2efae8b
Matthew Phelps <mdp553@hotmail.com>
2016-01-27 14:35:00 +0100

    In process: Get data JAGS ready

--- Temp-1.R
+++ Temp-1.R
@@ -21,10 +21,29 @@ library(ggplot2)
 load("Data_4.Rdata")
 
 
-
 # SUBSET to CHRISTIANIA ---------------------------------------------------
 
 I_chris <- I_multi_replicate[which(I_multi_replicate$quarter == "Christianshavn"), ]
 
 # Selecton only columns with "rep" in name. See: http://goo.gl/s9xRKr
 X_ch <- I_chris[, grepl("rep", names(I_chris))]
+
+
+
+# DATA PREP FOR STAN ------------------------------------------------------
+# Once method is tested, repeat for all quarters
+Nsteps <- nrow(I_chris)
+Nrep <- ncol(X_ch)
+S_it <- matrix(0, Nsteps, Nrep)
+I_it <- matrix(0, Nsteps, Nrep)
+N_i  <- matrix(0, Nsteps, Nrep)
+
+for (i in 1:Nrep){
+  for( t in 1:Nsteps){
+    S_it[t, i] <- (combined$S[which(combined$quarterID==i)])[t]
+    I_it[t, i] <- (combined$sick.total.week[which(combined$quarterID==i)])[t]
+    N_i[t, i] <- (combined$est.pop.1853[which(combined$quarterID==i)])[t]
+  }
+}
+row.names(I_it) <- q_names[, 1]
+
