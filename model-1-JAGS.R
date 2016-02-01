8f0be4668ea1211c2fe39bf9be8df9815351e80c
Matthew Phelps <mdp553@hotmail.com>
2016-02-01 11:36:33 +0100

    Working on simulation
    Getting variables saved correctly, and simulation code ready

--- model-1-JAGS.R
+++ model-1-JAGS.R
@@ -3,9 +3,10 @@
 # Dependicies: Data 1, Data 2, 5_GLM_data_reshape, Data-3
 # Intro -------------------------------------------------------------------
 graphics.off()
-mac <- "/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH"
-pc <- "C:\\Users\\wrz741\\Google Drive\\Copenhagen\\DK Cholera\\CPH"
-setwd(pc)
+ifelse(grepl("wrz741", getwd()),
+       wd.path <- "C:\\Users\\wrz741\\Google Drive\\Copenhagen\\DK Cholera\\CPH",
+       wd.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH")
+setwd(wd.path)
 rm(list = ls())
 library(plyr)
 library(coda)
@@ -14,7 +15,7 @@ library(runjags)
 library(rjags)
 library(mcmcplots)
 library(ggmcmc)
-options(mc.cores = (parallel::detectCores()-1 ))
+options(mc.cores = (parallel::detectCores()-2 ))
 
 
 # LOAD -------------------------------------------------------
@@ -36,49 +37,67 @@ dataList <- list(N_i_daily = N_i_daily,
 
 
 # JAGS 1 ------------------------------------------------------------------
-jags <- jags.model('Rcodes\\stan-model_Fitting-one-replicate.stan',
-                   data = dataList,
-                   n.chains = 1,
-                   n.adapt = 1000)
-
+# jags <- jags.model('Rcodes\\stan-model_Fitting-one-replicate.stan',
+#                    data = dataList,
+#                    n.chains = 1,
+#                    n.adapt = 1000)
+# 
 
 # JAGS 2 ------------------------------------------------------------------
 
 
-model_0_3_jags <- run.jags(model = 'Rcodes\\stan-model_Fitting-one-replicate.stan',
+model_1_jags <- run.jags(model = 'Rcodes\\stan-model_Fitting-one-replicate.stan',
                            method = 'parallel',
                            monitor = c('beta', 'phi'),
                            data = dataList,
                            n.chains = 5,
                            adapt = 1000,
-                           burnin = 5000,
-                           sample = 200000,
+                           burnin = 50000,
+                           sample = 100000,
                            thin = 3,
                            plots = T)
 
-model_0_3_coda = as.mcmc.list( model_0_3_jags )
+model_1_coda = as.mcmc.list( model_1_jags )
+model_1_coda <- model_1_coda
 
  
 
 # JAGS DIAGNOSTICS --------------------------------------------------------
 
-print(model_0_3_jags)
+print(model_1_jags)
+summary(model_1_coda)
 
-model_0_3_ggs <- ggs(model_0_3_coda)
-trace_beta <- ggs_traceplot(model_0_3_ggs, family = 'beta', simplify = .3) +
+model_1_ggs <- ggs(model_1_coda)
+trace_beta <- ggs_traceplot(model_1_ggs, family = 'beta', simplify = .3) +
   theme(legend.position = 'none')
-density_beta <- ggs_density(model_0_3_ggs, family = 'beta')
-running_beta <- ggs_running(model_0_3_ggs, family = 'beta')
+density_beta <- ggs_density(model_1_ggs, family = 'beta')
+running_beta <- ggs_running(model_1_ggs, family = 'beta')
+
+trace_phi <- ggs_traceplot(model_1_ggs, family = 'phi', simplify = .3)
+density_phi <- ggs_density(model_1_ggs, family = 'phi')
+running_phi <- ggs_running(model_1_ggs, family = 'phi')
+
+
+
+ # Get beta PE into human readible matrix --------------------------------
+
+model_1_out <-as.data.frame(print(model_1_jags))
+nrow(model_1_out)
+beta_summary_1_ <- model_1_out[1, ]
+phi_summary_1_ <- model_1_out[2, ]
+
+# SAVE --------------------------------------------------------------------
+
+save(model_1_coda, file = "data\\Rdata\\model-1-mcmc-output.Rdata")
+
+save(beta_summary_1_, file = 'data\\Rdata\\beta-summary-1.Rdata')
+save(phi_summary_1_, file = 'data\\Rdata\\phi-summary-1.Rdata')
+save(dataList, file = 'data\\Rdata\\model-1-dataList.Rdata')
 
 ggsave(trace_beta, filename = "Output\\MCMC\\trace-beta-model-1.jpg")
 ggsave(density_beta, filename = 'Output\\MCMC\\density-beta-model-1.jpg')
 ggsave(running_beta, filename = 'Output\\MCMC\\running-beta-model-1.jpg')
 
-trace_phi <- ggs_traceplot(model_0_3_ggs, family = 'phi', simplify = .3)
-density_phi <- ggs_density(model_0_3_ggs, family = 'phi')
-running_phi <- ggs_running(model_0_3_ggs, family = 'phi')
-
-ggsave(trace_phi, filename = "Output\\MCMC\\trace_phi_0_3.jpg")
-ggsave(density_phi, filename = 'Output\\MCMC\\density_phi_0_3.jpg')
-ggsave(running_phi, filename = 'Output\\MCMC\\running_phi_0_3.png')
-
+ggsave(trace_phi, filename = "Output\\MCMC\\trace-phi-1.jpg")
+ggsave(density_phi, filename = 'Output\\MCMC\\density-phi-1.jpg')
+ggsave(running_phi, filename = 'Output\\MCMC\\running-phi-1.png')
