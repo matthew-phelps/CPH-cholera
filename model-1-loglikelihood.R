4694c6bca40047315f0198120476533fad2065e9
Matthew Phelps <mdp553@hotmail.com>
2016-02-17 09:44:08 +0100

    Simplify variables in code for better readability
    Superficial changes, underlying functionality not changes

--- model-1-loglikelihood.R
+++ model-1-loglikelihood.R
@@ -19,17 +19,18 @@ require(grid)
 
 # load(file = 'data\\Rdata\\I_fitted_phi.Rdata')
 # load(file = 'data\\Rdata\\I_fake_phi.Rdata')
-load(file = 'data\\Rdata\\I_incidence.Rdata')
 # load(file = 'data\\Rdata\\I_phi_vect.Rdata')
+# load(file = 'data\\Rdata\\I_fake_plus1_phi.Rdata')
+load(file = 'data\\Rdata\\I_incidence.Rdata')
 load(file = 'data\\Rdata\\phi_vect.Rdata')
 load(file = 'data\\Rdata\\I_fit_plus1_phi.Rdata')
-# load(file = 'data\\Rdata\\I_fake_plus1_phi.Rdata')
 load(file = 'data\\Rdata\\I_phi_plus1_vect.Rdata')
 
+phi_rez <- length(I_phi_plus1_vect)
+num_loops <- nrow(I_fit_plus1_phi)
 
 # Just check to make sure we're dealing with the correct data set
 plot(I_incidence)
-# plot(I_fake_phi[[2]][1:112])
 plot(I_fit_plus1_phi[2, 1:112])
 
 
@@ -42,8 +43,6 @@ I_fit_plus1_phi_60 <- I_fit_plus1_phi[, 20:65]
 # I_fitted_phi_60 <- list()
 # I_phi_vect_60 <- I_phi_vect
 # I_fake_plus1_phi_60 <- list()
-I_phi_plus1_vect_60 <- I_phi_plus1_vect
-
 # for(loops in 1:length(I_fit_plus1_phi)){
 #   #   I_fake_phi_60[[z]] <- I_fake_phi[[z]][20:65]
 #   #   I_fitted_phi_60[[z]] <- I_fitted_phi[[z]][1 ,20:65]
@@ -51,13 +50,12 @@ I_phi_plus1_vect_60 <- I_phi_plus1_vect
 #   # I_fake_plus1_phi_60[[z]] <- I_fake_plus1_phi[[z]][20:65]
 # } 
 
+
+I_phi_plus1_vect_60 <- I_phi_plus1_vect
 #Separate for-loop for phi-vector since it is a nested list
 for(vect in 1:length(I_phi_plus1_vect)){
   I_phi_plus1_vect_60[[vect]]<- I_phi_plus1_vect[[vect]][, 20:65]
 }
-
-
-
 
 rm(I_incidence, I_fake_phi, I_fitted_phi, I_fake_plus1_phi,
    I_phi_plus1_vect, I_phi_vect, I_fit_plus1_phi)
@@ -103,11 +101,11 @@ plot(I_phi_plus1_vect_60[[120]][2,])
 
 
 # MLE FULL SIM for vector of Phis --------------------------------------------------
-rm(list = setdiff(ls(), c("I_phi_vect_60", 
-                          "I_incidence_60",
+rm(list = setdiff(ls(), c("I_incidence_60",
                           "I_fit_plus1_phi_60",
                           "I_phi_plus1_vect_60",
-                          "I_fake_plus1_phi_60",
+                          "phi_rez",
+                          "num_loops",
                           "phi_pe"))) #http://goo.gl/88L5C2
 
 
@@ -175,13 +173,14 @@ rm(list = setdiff(ls(), c("I_phi_vect_60",
 # MLE 1-STEP-AHEAD -------------------------------------------------------
 
 ll_t <- vector("list", length(I_phi_plus1_vect_60))
-ll_row_sums <- vector("list", length(I_phi_plus1_vect_60))
+ll_row_sums <- matrix(data = NA, nrow = num_loops, ncol = phi_rez)
+
 ll_phi_avg <- vector(length = length(I_phi_plus1_vect_60))
-model_ll_phi_plus1_vect <- matrix(data = NA, nrow = 1, ncol = length(I_phi_plus1_vect_60))
-for(vect in 1:length(I_phi_plus1_vect_60)){
+
+for(vect in 1:phi_rez){
   ll_t[[vect]] <- dpois(I_incidence_60, I_phi_plus1_vect_60[[vect]], log = T)
-  ll_row_sums[[vect]] <- rowSums(ll_t[[vect]])
-  ll_phi_avg[vect] <- mean(ll_row_sums[[250]])
+  ll_row_sums[, vect] <- exp(rowSums(ll_t[[vect]]))
+  ll_phi_avg[vect] <- mean(ll_row_sums[, vect])
 }
 
 
@@ -193,7 +192,7 @@ for(vect in 1:length(I_phi_plus1_vect_60)){
 #   }
 #   model_ll_phi_plus1_vect[1, vect] <- (mean(ll_z))
 # }
-
+model_ll_phi_plus1_vect <- matrix(data = NA, nrow = 1, ncol = phi_rez)
 model_ll_phi_plus1_vect <- rbind(ll_phi_avg, phi_pe)
 model_ll <- data.frame(t(model_ll_phi_plus1_vect))
 # plot(model_ll_phi_plus1_vect[2,], model_ll_phi_plus1_vect[1,])
@@ -204,11 +203,11 @@ model_ll <- data.frame(t(model_ll_phi_plus1_vect))
 colnames(model_ll) <- c("LL", "phi")
 
 # Dynamic subtitle to reflect number of loops
-no_loops <- as.character(length(I_fit_plus1_phi_60))
+no_loops <- as.character(nrow(I_fit_plus1_phi_60))
 sub_title <- paste("No. simulations = ", no_loops, sept = "")
 
 ll_plot <- ggplot(data = model_ll,
-                  aes(x = phi, y = (LL), label = phi)) +
+                  aes(x = phi, y = log(LL), label = phi)) +
   geom_point(size = 2) +
   #Add labels: http://goo.gl/pE9JPI
   geom_vline(xintercept = 0.0689, linetype = 2) +
