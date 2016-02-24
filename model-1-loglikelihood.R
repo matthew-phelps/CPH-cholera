57459cb37824159ed7515e2f423b623989a45194
Matthew Phelps <mdp553@hotmail.com>
2016-02-24 09:54:25 +0100

    Re-add single fake phi test (might be redundant now)
    Not sure if I still need to do likelihood testing in this manner. This
    whole script might be redundant.

--- model-1-loglikelihood.R
+++ model-1-loglikelihood.R
@@ -17,6 +17,7 @@ library(ggplot2)
 library(reshape)
 require(grid)
 
+
 # load(file = 'data\\Rdata\\I_fitted_phi.Rdata')
 # load(file = 'data\\Rdata\\I_fake_phi.Rdata')
 # load(file = 'data\\Rdata\\I_phi_vect.Rdata')
@@ -38,24 +39,17 @@ plot(I_fit_plus1_phi[2, 1:112])
 
 # Restrict likelihood calculation to first half of epidemic. Otherwise we see
 # -inf values for some reason:
-I_incidence_60 <- I_incidence[20:65]
-I_fit_plus1_phi_60 <- I_fit_plus1_phi[, 20:65]
-
-# I_fake_phi_60 <- list()
+# # I_fake_phi_60 <- list()
 # I_fitted_phi_60 <- list()
 # I_phi_vect_60 <- I_phi_vect
-I_fake_plus1_phi_60 <- list()
-# for(loops in 1:length(I_fit_plus1_phi)){
-#   #   I_fake_phi_60[[z]] <- I_fake_phi[[z]][20:65]
-#   #   I_fitted_phi_60[[z]] <- I_fitted_phi[[z]][1 ,20:65]
-#   
-#   # I_fake_plus1_phi_60[[z]] <- I_fake_plus1_phi[[z]][20:65]
-# } 
+I_incidence_60 <- I_incidence[20:65]
+I_fit_plus1_phi_60 <- I_fit_plus1_phi[, 20:65]
+I_fake_plus1_phi_60 <- I_fake_plus1_phi[, 20:65]
 
 
+#Separate for-loop for phi-vector since it is different structure
 I_phi_plus1_vect_60 <- I_phi_plus1_vect
 I_phi_plus1_vect_par_60 <- I_phi_plus1_vect_parallel
-#Separate for-loop for phi-vector since it is a nested list
 for(phi_value in 1:length(I_phi_plus1_vect_parallel)){
   # I_phi_plus1_vect_60[[phi_value]]<- I_phi_plus1_vect[[phi_value]][, 20:65]
   I_phi_plus1_vect_par_60[[phi_value]] <- I_phi_plus1_vect_parallel[[phi_value]][, 20:65]
@@ -134,8 +128,6 @@ rm(serial_1, par_1)
 
 # MLE FULL SIM for vector of Phis --------------------------------------------------
 
-
-
 # ll_t <- vector("list", length(I_phi_vect_60[[1]]))
 # ll_z <- vector(length = length(I_phi_vect_60[[1]]))
 # model_ll_phi_vect <- matrix(data = NA, nrow = 1, ncol = length(I_phi_vect_60))
@@ -168,41 +160,50 @@ rm(serial_1, par_1)
 #        units = 'cm')
 
 
-rm(list = setdiff(ls(), c("I_incidence_60",
-                          "I_fit_plus1_phi_60",
-                          "I_phi_plus1_vect_60",
-                          "I_phi_plus1_vect_par_60",
-                          "phi_rez",
-                          "num_loops",
-                          "phi_pe"))) #http://goo.gl/88L5C2
 
 ######################################################
 # 1_STEP_AHEAD SIMULATION
 # ####################################################
 
-# 
-# ll_t <- list()
-# ll_z <- 0
-# for(z in 1:length(I_fake_plus1_phi_60)){
-#   ll_t[[z]] <- dpois(I_incidence_60, I_fake_plus1_phi_60[[z]], log = T)
-#   ll_z[z] <- exp(sum(ll_t[[z]]))
-# }
-# 
-# model_ll_fake_phi <- sum(ll_z)
-# 
-# # Sum log-likelihood across each timestep for FITTED phi:
-# ll_t <- list()
-# ll_z <- 0
-# for(z in 1:length(I_fit_plus1_phi_60)){
-#   ll_t[[z]] <- dpois(I_incidence_60, I_fit_plus1_phi_60[[z]], log = T)
-#   ll_z[z] <- exp(sum(ll_t[[z]]))
-# }
-# 
-# model_ll_fitted_phi <- sum(ll_z)
-# 
-# 
-# # Compare. If fitted is better 
-# model_ll_fitted_phi > model_ll_fake_phi
+rm(list = setdiff(ls(), c("I_incidence_60",
+                          "I_fit_plus1_phi_60",
+                          "I_phi_plus1_vect_60",
+                          "I_phi_plus1_vect_par_60",
+                          "I_fake_plus1_phi_60",
+                          "phi_rez",
+                          "num_loops",
+                          "phi_pe"))) #http://goo.gl/88L5C2
+
+# LL for fake phi
+ll_t <- matrix(NA, nrow = num_loops, ncol = ncol(I_fake_plus1_phi_60))
+ll_sim_sum <- vector(length = num_loops)
+ll_t <- dpois(I_incidence_60, I_fake_plus1_phi_60, log = T)
+ll_sim_sum <- rowSums(ll_t)
+
+which.min(ll_sim_sum)
+I_fake_plus1_phi[106,]
+boxplot(ll_sim_sum)
+
+
+# LL for fitted phi:
+ll_t <- matrix(NA, nrow = num_loops, ncol = ncol(I_fit_plus1_phi_60))
+ll_sim_sum <- vector(length = num_loops)
+ll_t <- dpois(I_incidence_60, I_fit_plus1_phi_60, log = T)
+ll_sim_sum <- rowSums(ll_t)
+boxplot(ll_sim_sum)
+
+ll_t <- list()
+ll_z <- 0
+for(z in 1:length(I_fit_plus1_phi_60)){
+  ll_t[[z]] <- dpois(I_incidence_60, I_fit_plus1_phi_60[[z]], log = T)
+  ll_z[z] <- exp(sum(ll_t[[z]]))
+}
+
+model_ll_fitted_phi <- sum(ll_z)
+
+
+# Compare. If fitted is better
+model_ll_fitted_phi > model_ll_fake_phi
 
 
 # MLE 1-STEP-AHEAD SERIAL-------------------------------------------------------
