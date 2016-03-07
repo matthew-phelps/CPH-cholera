df2ac3680ad1b016d1643c6aba92f71fbe477746
Matthew Phelps <mdp553@hotmail.com>
2016-03-07 11:19:08 +0100

    Fit Christianshavn JAGS
    Add code to fit Christianshavn using same model as for St. Annae V.

--- Christianshavn/CHRIST-model-1-data-prep.R
+++ Christianshavn/CHRIST-model-1-data-prep.R
@@ -22,10 +22,9 @@ load("Data_4.Rdata")
 
 
 # SUBSET to 1 QUARTER ---------------------------------------------------
-quarter_chosen <- as.character("Christianshavn")
 
-I_qrt <- I_multi_replicate[which(I_multi_replicate$quarter == quarter_chosen), ]
-
+I_qrt <- I_multi_replicate[which(I_multi_replicate$quarter == "Christianshavn"), ]
+N_St_annae_v <- 15836
 # Selecton only columns with "rep" in name. See: http://goo.gl/s9xRKr
 X_qrt <- I_qrt[, grepl("rep", names(I_qrt))]
 
@@ -46,15 +45,15 @@ for (t in 2:nrow(X_qrt)){
 
 Nsteps <- nrow(cum_qrt)
 Nrep <- ncol(X_qrt)
-N_quarter_chosen <- 15836
+
 S_it_daily <- matrix(0, Nsteps, Nrep)
 I_incidence <- X_qrt
 N_i_daily  <- matrix(0, Nsteps, Nrep)
-N_i_daily <- N_quarter_chosen
+N_i_daily <- N_St_annae_v
 
 for (rep in 1:Nrep){
   for( t in 1:Nsteps){
-    S_it_daily[t, rep] <- N_quarter_chosen - cum_qrt[t, rep]
+    S_it_daily[t, rep] <- N_St_annae_v - cum_qrt[t, rep]
   }
 }
 
@@ -70,17 +69,41 @@ rm(list = setdiff(ls(), c("I_incidence", "N_i_daily", "Nsteps",
 # DATA SHAPE --------------------------------------------------------------
 # Create separate vectors for several replicates
 # These will be fit separately in JAGS
-num_replicates <- 9
-I_rep <- data.frame(matrix(NA, nrow = Nsteps , ncol = num_replicates))
-S_rep <- data.frame(matrix(NA, nrow = Nsteps, ncol = num_replicates))
+I_rep1 <- I_incidence[, 1]
+I_rep2 <- I_incidence[, 2]
+I_rep3 <- I_incidence[, 3]
+I_rep4 <- I_incidence[, 4]
+I_rep5 <- I_incidence[, 5]
+I_rep6 <- I_incidence[, 6]
+I_rep7 <- I_incidence[, 7]
+I_rep8 <- I_incidence[, 8]
+I_rep9 <- I_incidence[, 9]
+I_rep10 <- I_incidence[, 10]
+I_reps <- list(I_rep1, I_rep2, I_rep3, I_rep4,
+               I_rep5, I_rep6, I_rep7, I_rep8,
+               I_rep9, I_rep10)
+# Sanity check:
+plot(I_rep2, type = "l")
+lines(I_rep1, col = "red")
 
-for (i in 1:num_replicates){
-  I_rep[, i] <- I_incidence[, i]
-  S_rep[, i] <- S_it_daily[, i]
-}
-
-
+S_rep1 <- S_it_daily[, 1]
+S_rep2 <- S_it_daily[, 2]
+S_rep3 <- S_it_daily[, 3]
+S_rep4 <- S_it_daily[, 4]
+S_rep5 <- S_it_daily[, 5]
+S_rep6 <- S_it_daily[, 6]
+S_rep7 <- S_it_daily[, 7]
+S_rep8 <- S_it_daily[, 8]
+S_rep9 <- S_it_daily[, 9]
+S_rep10 <- S_it_daily[, 10]
+S_reps <- list(S_rep1, S_rep2, S_rep3, S_rep4,
+               S_rep5, S_rep6, S_rep7, S_rep8,
+               S_rep9, S_rep10)
+plot(S_rep1, type = "l")
+lines(S_rep2, col = "red")
 # SAVE --------------------------------------------------------------------
 
-rm(I_incidence, S_it_daily)
-save(list = ls(), file = "model-1-data-prep_CHRIST.Rdata")
+rm(list = setdiff(ls(), c("I_reps", "N_i_daily", "Nsteps",
+                          "S_reps"))) #http://goo.gl/88L5C2
+
+save(list = ls(), file = "CHRIST-model-1-data-prep.Rdata")
