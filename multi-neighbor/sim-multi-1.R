d941aac79ae517b237eb57de765a9b164f75a9eb
Matthew Phelps <Matthew@Matthews-MBP.wireless.yale.internal>
2016-05-10 10:40:34 -0400

    Store attributable cases during simulation
    Allows for calculation of the number of attributable cases for each
    neighborhood pair at each timestep

--- multi-neighbor/sim-multi-1.R
+++ multi-neighbor/sim-multi-1.R
@@ -5,11 +5,16 @@
 
 # Intro -------------------------------------------------------------------
 graphics.off()
-ifelse(grepl("wrz741", getwd()),
-       wd.path <- "C:\\Users\\wrz741\\Google Drive\\Copenhagen\\DK Cholera\\CPH\\Data\\Rdata",
-       wd.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Data/Rdata")
-setwd(wd.path)
 rm(list = ls())
+ifelse(grepl("wrz741", getwd()),
+       wd.path <- "C:/Users/wrz741/Google Drive/Copenhagen/DK Cholera/CPH/Data/Rdata",
+       wd.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Data/Rdata")
+ifelse(grepl("wrz741", getwd()),
+       save.plot.path <- "C:/Users/wrz741/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/Multi",
+       save.plot.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/Multi")
+
+setwd(wd.path)
+
 
 library(ggplot2)
 library(tidyr)
@@ -32,14 +37,15 @@ city_obs_2 <- data.frame(t(I_it))
 colnames(city_obs_2) <- q_names
 city_obs_2$day_index <- seq(from = 7, to = (Nweeks )* 7, length.out = Nweeks)
 city_obs_2$I <- rowSums(city_obs_2[, 1:9]) / 7
+quarter_sums <- select(combined, c(quarter, week.id, cum.sick))
+quarter_sums <- quarter_sums[quarter_sums$week.id == 15, c(1, 3)]
 
-duration <- 5 # In days. "1-2 weeks" from DOI:  10.1038/nrmicro2204
-gamma <- 1/duration
 
-# LOOPS -------------------------------------------------------------------
+# GLOBAL VARIABLES -------------------------------------------------------------------
 
 loops <- 1000 # Has to be the same for both full sum and t+1 sim
-
+duration <- 5 # In days. "1-2 weeks" from DOI:  10.1038/nrmicro2204
+gamma <- 1/duration
 
 
 
@@ -51,8 +57,15 @@ R_new <-          matrix(nrow = Nsteps, ncol = Nquarter)
 I_new <-          matrix(nrow = Nsteps, ncol = Nquarter)
 I_sim_vect <-     matrix(nrow = Nsteps, ncol = Nquarter)
 S_it_est <-       matrix(nrow = Nsteps, ncol = Nquarter)
-I_new_mat <- list(data.frame(matrix(data = NA, nrow = Nsteps, ncol = Nquarter)))
 
+Lambda_quart <-   list(data.frame(matrix(nrow = Nsteps, ncol = Nquarter)))
+I_new_mat <- list(data.frame(matrix(data = NA, nrow = Nsteps, ncol = Nquarter)))
+# Attributable infections variables:
+Lambda_quart<-lapply(1:Nquarter,
+                     function(x) data.frame(matrix(NA,
+                                                   nrow=Nsteps,
+                                                   ncol=Nquarter)))
+I_attr <-list((matrix(data = NA, nrow = Nquarter, ncol = Nquarter)))
 # Starting values
 I_sim_vect[1, ] <- I_it_daily[1]
 I_sim_vect[1, c(5, 8, 9)] <- 1 # Init Nyboder with 2 cases
@@ -64,34 +77,61 @@ set.seed(13)
 for (z in 1:loops){
   for (t in 1:(Nsteps-1)){
     for(i in 1:Nquarter){
-      Lambda_est_pe[t, i] <- S_it_est[t, i] / N_it[i] * sum( betas[, i] * I_sim_vect[t, ] )
+      
+      Lambda_est_pe[t, i] <- S_it_est[t, i] / N_it[i] * sum( betas[, i] * I_sim_vect[t, ])
       LambdaR[t, i] <- I_sim_vect[t, i] * gamma
       R_new[t, i] <- rpois(1, LambdaR[t, i])
       I_new[t, i] <- rpois(1, (Lambda_est_pe[t, i] ) )
       I_sim_vect[t + 1, i] <- max(0, (I_new[t, i] + I_sim_vect[t, i] - R_new[t, i]))
       S_temp <- (S_it_est[t, i]) -    (I_new[t, i]) / (phi)
       S_it_est[t + 1, i] <- max(0, S_temp)
+      Lambda_quart[[i]][t, ] <- (S_it_est[t, i] / N_it[i]) * ( betas[, i] * I_sim_vect[t, ])
     }
   }
+  # For each quarter: store sum of infections attributed to each quarter over
+  # all time-steps
+  I_attr[[z]] <- sapply(Lambda_quart, colSums, na.rm=T)
+  # Store new infections at each timestep for each quarter for each simulation
   I_new_mat[[z]] <- data.frame(I_new)
-  I_new_mat[[z]]$sim_sum <- z
+  I_new_mat[[z]]$sim_num <- z
 }
 proc.time() - ptm
 
 
 
 
+
+x <- 2
+I_new_mat[[x]]
+I_attr[[x]]
+m <- matrix(colSums(I_new_mat[[x]], na.rm = T))
+p <- matrix(colSums(I_attr[[x]]) )
+m <- m[1:9]
+p-m
+p
+colSums(Lambda_quart[[x]], na.rm = T)
+
+
+
+
+
+
+
+
 # T = 0 : DATA RESHAPE --------------------------------------------------------------
 
-# Simulated daily incidence to long format:
+# Simulated daily for each quarter
 x <- do.call(rbind.data.frame, I_new_mat) # merge all sims to 1 df
 colnames(x) <- c(q_names, "sim_num")
-x$day <- 1:112
-I_simulated <- gather(x, quarter, I_simulated, 1:9) # wide to long
+x$day <- 1:Nsteps
+I_simulated <- gather(x, quarter, I_simulated, 1:Nquarter) # wide to long
+
+# Observed data
 I_obs_df <- data.frame(I_it_daily)
 colnames(I_obs_df) <- q_names
-I_obs_df$day <- 1:112
-I_obs_lng <- gather(I_obs_df, quarter, I_obs, 1:9)
+I_obs_df$day <- 1:Nsteps
+I_obs_lng <- gather(I_obs_df, quarter, I_obs, 1:Nquarter)
+rm(x)
 
 
 # T = 0: PLOT PANEL QUARTERS-------------------------------------------------------------
@@ -100,21 +140,16 @@ sim1_plot <- ggplot() +
   geom_line(data = I_simulated,
             alpha = 0.01,
             aes(x = day, y = I_simulated,
-                                    group = interaction(quarter, sim_num),
-                                    color = quarter)) +
+                group = interaction(quarter, sim_num),
+                color = quarter)) +
   geom_line(data = I_obs_lng, aes(x = day,
-                                   y = I_obs, group = quarter)) +
+                                  y = I_obs, group = quarter)) +
   facet_wrap(~quarter) +
   theme_minimal() +
   theme(legend.position = "none") +
   ggtitle("Simulation from t = 0")
 sim1_plot
-ggsave(filename = '/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/multi/Sim-1-quarter.png',
-       plot = sim1_plot,
-       width = 26,
-       height = 16,
-       units = 'cm',
-       dpi = 300)
+
 
 
 # T = 0: Citywide ---------------------------------------------------------
@@ -136,12 +171,44 @@ city1_plot <- ggplot() +
   ggtitle("Simulated from t = 0 \naggregated to city level")
 city1_plot
 
-ggsave(filename = '/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/multi/Sim-1-citywide.png',
+
+# T = 0: Attributable cases -----------------------------------------------
+# Element-wise mean of list of matrices. From : http://goo.gl/VA7S66
+I_att_mean <- data.frame(Reduce("+", I_attr) / length(I_attr), row.names = q_names)
+colnames(I_att_mean) <- q_names
+
+I_proportion <- data.frame(matrix(data = NA, nrow = Nquarter, ncol = Nquarter), row.names = q_names)
+colnames(I_proportion) <- q_names
+for (i in 1:Nquarter){
+  for (j in 1:Nquarter){
+    I_proportion[j, i] <- I_att_mean[j, i] / quarter_sums[i, 2]
+      
+  }
+}
+
+
+# T = 0: Save -------------------------------------------------------
+
+setwd(save.plot.path)
+ggsave(filename = 'Sim-1-quarter.png',
+       plot = sim1_plot,
+       width = 26,
+       height = 16,
+       units = 'cm',
+       dpi = 300)
+
+ggsave(filename = 'Sim-1-citywide.png',
        plot = city1_plot,
        width = 26,
        height = 16,
        units = 'cm',
        dpi = 300)
+
+setwd(wd.path)
+save(I_att_mean, file = "Attributable-cases-t0.Rdata")
+save(I_proportion, file = "Proportion-attributable-t0.Rdata")
+
+
 
 
 ###############################################################################
@@ -158,6 +225,15 @@ I_new <-          matrix(nrow = Nsteps, ncol = Nquarter)
 I_sim_vect <-     matrix(nrow = Nsteps, ncol = Nquarter)
 S_plus1_mat <-    matrix(nrow = Nsteps, ncol = Nquarter)
 I_new_plus1 <- list(data.frame(matrix(data = NA, nrow = Nsteps, ncol = Nquarter)))
+# Attributable infections variables:
+Lambda_quart<-lapply(1:Nquarter,
+                     function(x) data.frame(matrix(NA,
+                                                   nrow=Nsteps,
+                                                   ncol=Nquarter)))
+I_attr <-lapply(1:loops,
+                function(x) data.frame(matrix(NA,
+                                              nrow=Nquarter,
+                                              ncol=Nquarter)))
 
 # Starting values
 I_sim_vect[1, ] <- I_it_daily[1]
@@ -178,8 +254,12 @@ for (z in 1:loops){
       I_sim_vect[t + 1, i] <- max(0, (I_it_daily[t, i] + I_sim_vect[t, i] - R_new[t, i]))
       S_temp <- (S_plus1_mat[t, i]) -    (I_new[t, i]) / (phi)
       S_plus1_mat[t + 1, i] <- max(0, S_temp)
+      Lambda_quart[[i]][t, ] <- (S_plus1_mat[t, i] / N_it[i]) * ( betas[, i] * I_sim_vect[t, ])
     }
   }
+  # For each quarter: store sum of infections attributed to each quarter over
+  # all time-steps
+  I_attr[[z]] <- sapply(Lambda_quart, colSums, na.rm=T)
   I_new_plus1[[z]] <- data.frame(I_new)
   I_new_plus1[[z]]$sim_sum <- z
 }
@@ -199,6 +279,22 @@ I_obs_df$day <- 1:112
 I_obs_lng <- gather(I_obs_df, quarter, I_obs, 1:9)
 
 
+# T + 1: Attributable cases -----------------------------------------------
+# Element-wise mean of list of matrices. From : http://goo.gl/VA7S66
+I_att_mean_plus1 <- data.frame(Reduce("+", I_attr) / length(I_attr), row.names = q_names)
+colnames(I_att_mean_plus1) <- q_names
+
+I_proportion_plus1 <- data.frame(matrix(data = NA, nrow = Nquarter, ncol = Nquarter), row.names = q_names)
+colnames(I_proportion_plus1) <- q_names
+for (i in 1:Nquarter){
+  for (j in 1:Nquarter){
+    I_proportion_plus1[j, i] <- I_att_mean_plus1[j, i] / quarter_sums[i, 2]
+    
+  }
+}
+
+
+
 
 # T + 1: PLOT PANEL QUARTERS-------------------------------------------------------------
 
@@ -216,12 +312,6 @@ sim1_plus1 <- ggplot() +
   ggtitle("Simulation t + 1")
 sim1_plus1
 
-ggsave(filename = '/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/multi/Sim-1-plus1-quarter.png',
-       plot = sim1_plus1,
-       width = 26,
-       height = 16,
-       units = 'cm',
-       dpi = 300)
 
 
 # T + 1: Citywide ---------------------------------------------------------
@@ -243,9 +333,28 @@ city_plus1_plot <- ggplot() +
   ggtitle("Simulated t + 1\naggregated to city level")
 city_plus1_plot
 
+
+
+
+
+# T + 1: Save -------------------------------------------------------
+
+setwd(save.plot.path)
+
+ggsave(filename = '/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/multi/Sim-1-plus1-quarter.png',
+       plot = sim1_plus1,
+       width = 26,
+       height = 16,
+       units = 'cm',
+       dpi = 300)
+
 ggsave(filename = '/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/multi/Sim-1-plus1-citywide.png',
        plot = city_plus1_plot,
        width = 26,
        height = 16,
        units = 'cm',
        dpi = 300)
+
+save(I_att_mean_plus1, file = "Attributable-cases-tplus1.Rdata")
+save(I_proportion_plus1, file = "Proportion-attributable-tplus1.Rdata")
+
