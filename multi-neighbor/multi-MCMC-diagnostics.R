1dc2760a14c04dccd380dc48cfe9f378c31157bc
Matthew Phelps <Matthew@Matthews-MacBook-Pro.local>
2016-05-13 16:16:20 -0400

    Update diagnostics with latest extension
    This are extensions of the “full” model

--- multi-neighbor/multi-MCMC-diagnostics.R
+++ multi-neighbor/multi-MCMC-diagnostics.R
@@ -24,62 +24,66 @@ library(coda)
 library(ggmcmc)
 # LOAD --------------------------------------------------------------------
 
-load("JAGS-rep-5_ext2.Rdata")
-load("JAGS-rep-4-ext2.Rdata")
-load("JAGS-rep-6-ext-2.Rdata")
+load("jags-rep-5-ext-4.Rdata")
+load("jags-rep-4-ext-4.Rdata")
+load("JAGS-rep-6-ext-4.Rdata")
+jags_list <- list(jags_rep_4_ext4,
+                  jags_rep_5_ext4,
+                  jags_rep_6_ext4)
+jags_list_drop_6 <- list(jags_rep_4_ext4,
+                  jags_rep_5_ext4)
+
 setwd(rdata.path)
 load("sim-multi-1-data.Rdata")
 rm(I_it_daily, N_it, weekly_avg)
 
-
-model_5_mcmc <- as.mcmc.list(jags_rep_5_ext2)
+model_5_mcmc <- as.mcmc.list(jags_rep_5_ext4)
 model_5_ggs <- ggs(model_5_mcmc)
 
-model_4_mcmc <- as.mcmc.list(jags_rep_4_ext2)
+model_4_mcmc <- as.mcmc.list(jags_rep_4_ext4)
 model_4_ggs <- ggs(model_4_mcmc)
 
-model_6_mcmc <- as.mcmc.list(jags_rep_6_ext2)
+model_6_mcmc <- as.mcmc.list(jags_rep_6_ext4)
 model_6_ggs <- ggs(model_6_mcmc)
 
 
-
-
+x$density <- add.summary(jags_rep_5_ext3)
+names(jags_rep_5_ext3)
+jags_rep_5_ext3$density
 # COMBINE MCMC ------------------------------------------------------------
 
 # First divide into sets of 4 chains:
 
-rep5_1 <- divide.jags(jags_rep_5_ext2,
-                      which.chains = 1:4)
-rep5_2 <- divide.jags(jags_rep_5_ext2,
-                      which.chains = 5:8)
-
-mcmc_tot_sep <- combine.mcmc(mcmc.objects =  list(jags_rep_6_ext2,
-                                                  rep5_1,
-                                                  rep5_2,
-                                                  jags_rep_4_ext2), 
+mcmc_tot_sep <- combine.mcmc(mcmc.objects =  jags_list,
                              add.mutate = F,
                              collapse.chains = F)
 
-mcmc_tot <- combine.mcmc(mcmc.objects =  list(jags_rep_6_ext2,
-                                              rep5_1,
-                                              rep5_2,
-                                              jags_rep_4_ext2), 
+mcmc_tot <- combine.mcmc(mcmc.objects =  jags_list, 
+                         add.mutate = F,
+                         collapse.chains = T)
+
+mcmc_tot <- combine.mcmc(mcmc.objects =  jags_list, 
+                         add.mutate = F,
+                         collapse.chains = T)
+
+
+mcmc_tot_drop_6 <- combine.mcmc(mcmc.objects =  jags_list_drop_6, 
                          add.mutate = F,
                          collapse.chains = T)
 
 total_ggs <- ggs(mcmc_tot_sep)
-# 1 - DIAGNOSTICS ---------------------------------------------------------
-mcmcplot(mcmc_tot_sep)
-add.summary(jags_rep_4_ext2 )
-#mcmcplot(jags_rep_5_ext1)
-gelman.plot(model_4_mcmc)
-gelman.diag(model_4_mcmc)
-mcmcplot(combine.mcmc(jags_rep_6_ext2, collapse.chains = F))
-mcmcplot(combine.mcmc(jags_rep_4_ext2, collapse.chains = F))
+
 
 
 # 95% CREDIBLE INTERVAL ---------------------------------------------------
 int_hpd <- data.frame(HPDinterval(mcmc_tot, 0.95))
+int_hpd_drop_6 <- data.frame(HPDinterval(mcmc_tot_drop_6, 0.95))
+rep_4hpd <- data.frame(HPDinterval(model_4_mcmc))
+rep_5hpd <- data.frame(HPDinterval(model_5_mcmc))
+rep_6hpd <- data.frame(HPDinterval(model_6_mcmc))
+min(rep_6hpd[22, ])
+min(rep_5hpd[22,])
+min(rep_4hpd[22,])
 
 mcmc_tot_df <- (t(mcmc_tot))
 n_draw <- ncol(mcmc_tot_df)
@@ -93,11 +97,11 @@ for (i in 1:n_par) {
   # instead of looping over every element in df, capitalize on R's vectorized
   # operations: http://goo.gl/eiCciz
   post_draws_full[i, ] <- ifelse(mcmc_tot_df[i, ] > int_hpd[i, 1],
-                            mcmc_tot_df[i, ],
-                            NA)
+                                 mcmc_tot_df[i, ],
+                                 NA)
   post_draws_full[i, ] <- ifelse(post_draws_full[i, ] < int_hpd[i, 2],
-                            mcmc_tot_df[i, ],
-                            NA)
+                                 mcmc_tot_df[i, ],
+                                 NA)
 }
 
 # Remove any mcmc draws that had ANY parameter outside the 95% CI:
@@ -110,9 +114,37 @@ n_samples <- ncol(post_draws)
 setwd(rdata.path)
 save(post_draws, file = "post_draws.Rdata")
 save(int_hpd, file = "int_hpd.Rdata")
+save(int_hpd_drop_6, file = "int_hpd_drop_6.Rdata")
+
+# 1 - HTML DIAGNOSTICS ---------------------------------------------------------
+mcmcplot(mcmc_tot_sep)
+add.summary(jags_rep_4_ext3 )
+
+gelman.plot(model_4_mcmc)
+gelman.diag(model_4_mcmc)
+mcmcplot(combine.mcmc(jags_rep_6_ext3, collapse.chains = F))
+mcmcplot(combine.mcmc(jags_rep_4_ext3, collapse.chains = F))
+mcmcplot(combine.mcmc(jags_rep_5_ext3, collapse.chains = F))
+
+
+# Log transform trace plots
+log_rep4 <- jags_rep_4_ext3
+log_rep5 <- jags_rep_5_ext3
+log_rep6 <- jags_rep_6_ext3
+nchains <- length(jags_rep_4_ext3$mcmc)
+for (i in 1:nchains){
+  log_rep4$mcmc[[i]] <- log(jags_rep_4_ext3$mcmc[[i]])
+  log_rep5$mcmc[[i]] <- log(jags_rep_5_ext3$mcmc[[i]])
+  log_rep6$mcmc[[i]] <- log(jags_rep_6_ext3$mcmc[[i]])
+}
+mcmcplot(combine.mcmc(log_rep4, collapse.chains = F))
+mcmcplot(combine.mcmc(log_rep5, collapse.chains = F))
+mcmcplot(combine.mcmc(log_rep6, collapse.chains = F))
+
 
 # 2 - GGS DIAGS -----------------------------------------------------------
 setwd(out.path)
+mcmcplot(jags_rep_4_ext3)
 
 den <- ggs_density(model_4_ggs, family = 'beta') +
   theme(legend.position = 'none')
@@ -135,7 +167,7 @@ ggsave(catp, file = "betas-catapillar.png",
        limitsize = F)
 
 
-beta_trace <- ggs_traceplot(model_4_ggs, family = 'beta', simplify = 0) +
+beta_trace <- ggs_traceplot(model_4_ggs, family = 'beta') +
   theme(legend.position = 'none')
 ggsave(beta_trace, file = "betas-trace.jpg",
        width = 20,
@@ -144,7 +176,10 @@ ggsave(beta_trace, file = "betas-trace.jpg",
        dpi = 150,
        limitsize = F)
 
-rhat <- ggs_Rhat(model_4_ggs) + xlab("R_hat")
+ggs_Rhat(model_4_ggs) + xlab("Rep 4 R_hat") + ggtitle("Rep 4")
+ggs_Rhat(model_5_ggs) + xlab("Rep 5 R_hat") + ggtitle("Rep 5")
+ggs_Rhat(model_6_ggs) + xlab("Rep 6 R_hat") + ggtitle("Rep 6")
+
 ggsave(rhat, file = "rhat2.jpg",
        width = 26,
        height = 40,
@@ -152,7 +187,7 @@ ggsave(rhat, file = "rhat2.jpg",
        dpi = 150,
        limitsize = F)
 
-gelman.plot(model_4_mcmc)
+gelman.plot(model_6_mcmc)
 gelman.diag(model_4_mcmc)
 
 
