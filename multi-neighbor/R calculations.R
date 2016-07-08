67fd926730765be0e9d47b4e5e722ba5db53b317
Matthew Phelps <mdp553@hotmail.com>
2016-07-08 13:41:38 +0200

    Add fitted gamma for R0 calculations

--- multi-neighbor/R calculations.R
+++ multi-neighbor/R calculations.R
@@ -10,8 +10,8 @@ ifelse(grepl("wrz741", getwd()),
        wd.path <- "C:/Users/wrz741/Google Drive/Copenhagen/DK Cholera/CPH/Data/Rdata",
        wd.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Data/Rdata")
 ifelse(grepl("wrz741", getwd()),
-       save.path <- "C:/Users/wrz741/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/Multi",
-       save.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Output/Simulations/Multi")
+       save.path <- "C:/Users/wrz741/Google Drive/Copenhagen/DK Cholera/CPH/Output/Results",
+       save.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Output/Results")
 
 setwd(wd.path)
 
@@ -24,10 +24,16 @@ library(CholeraDataDK)
 
 # LOAD data ---------------------------------------------------------------
 load(file = "int_hpd.Rdata")
-load(file = "sim-model-5-data.Rdata") # update as more MCMCs are run
+#load(file = "sim-model-5-data.Rdata") # update as more MCMCs are run
+
+
+
+b <- T
+m3 <- F
+ifelse(m3, load("sim-model-3-data.Rdata"), load(file = "sim-model-5-data-b.Rdata"))
+ifelse(b, jags_m5_ls <- jags_m5_ls_b, NA)
 
 rm(I_it_daily, N_it, weekly_avg, phi, Nsteps)
-duration <- 1/5
 
 
 
@@ -53,10 +59,10 @@ x <- as.matrix(betas) # convert to matrix for diag() function to work
 B_int <- diag(x) # extract diagonals
 rm(x)
 
-R_int <- data.frame(B_int / duration )
+R_int <- data.frame(B_int / gamma )
 colnames(R_int) <- "R_value"
-R_int$lower <- B_int_low / duration
-R_int$upper <- B_int_hi /duration
+R_int$lower <- B_int_low / gamma
+R_int$upper <- B_int_hi /gamma
 R_int$quarter <- q_names
 R_int$R_type <- "int"
 R_int$quarter <- factor(R_int$quarter, levels = R_int$quarter[order(R_int$R_value)])
@@ -76,10 +82,10 @@ x <- as.matrix(betas)
 diag(x) <- NA
 B_ext <- rowSums(x, na.rm = T)
 
-R_ext <- data.frame(B_ext / duration)
+R_ext <- data.frame(B_ext / gamma)
 colnames(R_ext) <- "R_value"
-R_ext$lower <- B_ext_low / duration
-R_ext$upper <- B_ext_hi / duration
+R_ext$lower <- B_ext_low / gamma
+R_ext$upper <- B_ext_hi / gamma
 R_ext$quarter <- q_names
 R_ext$R_type <- "ext"
 R_ext$quarter <- factor(R_ext$quarter, levels = R_ext$quarter[order(R_int$R_value)])
@@ -89,14 +95,14 @@ R <- rbind(R_int, R_ext)
 
 # CATAPILER PLOT ----------------------------------------------------------
 pd <- position_dodge(0.4)
-ggplot(data = R) +
-  geom_hline(yintercept = 1, linetype = 3, color = "light grey") +
+r0_1 <- ggplot(data = R) +
+  geom_hline(yintercept = 1, linetype = 3, color = "black") +
   geom_point(aes(x = quarter, y = R_value, shape = R_type, color = R_type),
              position = pd,
              size = 1.9) +
   geom_errorbar(aes(x = quarter, ymin = lower, ymax = upper, color = R_type),
                 width = 0.4,
-                size = 0.5,
+                size = 0.8,
                 position = pd) +
   scale_y_log10(breaks = c(0.0000001,
                            0.000001,
@@ -121,18 +127,24 @@ ggplot(data = R) +
   theme(axis.text.x = element_text(angle = 45,
                                    hjust = 1,
                                    vjust = 1.0))
+r0_1
 
-
+setwd(save.path)
+ggsave(r0_1,
+       filename = 'R6 - r0 log scale.jpg',
+       width = 15,
+       height = 9,
+       units = 'in')
 # PLOT: NON-LOG SCALE -----------------------------------------------------
 pd <- position_dodge(0.4)
-ggplot(data = R) +
+r0_2 <- ggplot(data = R) +
   geom_hline(yintercept = 1, linetype = 3, color = "light grey") +
   geom_point(aes(x = quarter, y = R_value, shape = R_type, color = R_type),
              position = pd,
              size = 1.9) +
   geom_errorbar(aes(x = quarter, ymin = lower, ymax = upper, color = R_type),
                 width = 0.4,
-                size = 0.5,
+                size = 0.8,
                 position = pd) +
   theme_classic() +
   ggtitle("R intern & R extern values") + 
@@ -140,3 +152,12 @@ ggplot(data = R) +
                                    hjust = 1,
                                    vjust = 1.0))
 
+
+
+r0_2
+setwd(save.path)
+ggsave(r0_2,
+       filename = 'R7 - r0 model 5.jpg',
+       width = 15,
+       height = 9,
+       units = 'in')
