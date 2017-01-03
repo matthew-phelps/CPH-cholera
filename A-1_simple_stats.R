ea4ca6996ece8ac690a9d6e41d4639614efbe0fa
Matthew Phelps <mdp553@hotmail.com>
2017-01-03 16:42:51 +0100

    Update summations & move old code to bottom
    Add checks to check that different summation methods led to same result

--- A-1_simple_stats.R
+++ A-1_simple_stats.R
@@ -5,32 +5,26 @@
 rm(list = ls())  
 graphics.off()
 ifelse(grepl("wrz741", getwd()),
-       wd.path <- "C:/Users/wrz741/Google Drive/Copenhagen/DK Cholera/CPH/data/Rdata",
-       wd.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Data/Rdata")
+       wd.path <- "C:/Users/wrz741/Google Drive/Copenhagen/DK Cholera/CPH/Data",
+       wd.path <-"/Users/Matthew/Google Drive/Copenhagen/DK Cholera/CPH/Data")
 
 
 setwd(wd.path)
+library(plyr)
 library(rgdal)
 library(rgeos)
-library(plyr)
+library(tidyverse)
 library(dplyr)
 
 
 
 # LOAD DATA ---------------------------------------------------------------
 
-load(file = "quarter_combined.Rdata")
-load(file = "quarter_eng.Rdata")
+load(file = "Rdata/quarter_combined.Rdata")
+load(file = "Rdata/quarter_eng.Rdata")
+load(file = "Rdata/quarter_eng_secondary.Rdata")
 qu_ls <- split(combined, f = combined$quarter)
 q_names <- names(qu_ls)
-# load("Rdata/cholera_by_street.Rdata")
-# load("Rdata/quarter_eng.Rdata")
-# load('Rdata/age1855.Rdata')
-# load('Rdata/age1850.Rdata')
-# load('Rdata/age_mortality.Rdata')
-# 
-# city <-read.table('CPH cholera outbreak 1853.csv', header=T, sep=",")
-
 
 
 
@@ -48,6 +42,16 @@ peak <- data.frame(inx = NA)
 for(i in 1:length(qu_ls)){
   peak[i, ] <- which.max(qu_ls[[i]][, vec])
 }
+
+
+
+
+# Date of first infected case in each quarter
+start <- combined %>%
+  group_by(quarter) %>%
+  dplyr::summarize(start = 1 + week.id[min(which(sick.total.week >0))])
+
+combined <- left_join(combined, start, by="quarter")
 
 
 
@@ -89,6 +93,8 @@ case_summary <- quarter %>%
   dplyr::summarize(cases = sum(sick.total.week),
                    deaths = sum(dead.total.week),
                    outside = max(outside))
+case_summary
+sum(case_summary$cases)
 
 # Cases outside the city walls:
 in_out_cases <- case_summary %>%
@@ -97,32 +103,34 @@ in_out_cases <- case_summary %>%
   dplyr::summarize(cases = sum(cases),
                    deaths = sum(deaths))
 in_out_cases
+sum(in_out_cases$cases)
 in_out_cases$deaths / in_out_cases$cases
 
-
-hosp_poor_cases <- quarter %>%
+# Hopstial and poor-house cases
+hosp_poor_cases <- quarter_secondary %>%
   group_by(hosp_poor) %>%
   dplyr::summarize(cases = sum(sick.total.week),
                    deaths = sum(dead.total.week),
                    outside = max(outside))
 hosp_poor_cases
+sum(hosp_poor_cases$cases)
+
 hosp_poor_cases$deaths / hosp_poor_cases$cases
 
-# AGE_ADJUSTED MORTALITY -------------------------------------------------
 
-mortality_rates <- age_ts$age[1:22]
-mortality_rates <- as.data.frame(mortality_rates)
-mortality_rates$rate <- age_mortality$total_sick / age_ts$total1853[1:22]
-mortality_rates <- plyr::rename(mortality_rates, replace = c('mortality_rates' = 'age'))
+ 
+model_cases <- combined %>%
+  group_by(quarter) %>%
+  dplyr::summarise(cases = sum(sick.total.week),
+                   deaths = sum(dead.total.week))
 
-plot(mortality_rates)
-
-save(mortality_rates, file = 'Rdata/mortality_rates.Rdata')
-
+sum(model_cases$cases) == sum(combined$sick.total.week)
 
 
 # QUARTER SUMMARY DATA FRAME ---------------------------------------------------------
-quarter_summary <- quarter[which(quarter$week.id == 15),
+quarter_summary <- c %>%
+  dplyr::filter(week.id == 15)
+  quarter[which(quarter$week.id == 15),
                            c("quarter", "est.pop.1853", "cum.sick", "S")]
 
 
@@ -132,20 +140,20 @@ quarter_summary <- quarter[which(quarter$week.id == 15),
 
 
 # POPULATION DENSITY ------------------------------------------------------
-
-load("Rdata/quarter_eng.Rdata")
-quarter.sheet <- reshape::rename(quarter, replace = c("sick.total.week" = "I"))
+# 
+# load("Rdata/quarter_eng.Rdata")
+# quarter.sheet <- reshape::rename(quarter, replace = c("sick.total.week" = "I"))
 
 # shapefile
-quarter.shp <- readOGR(dsn = "GIS", layer = "CPH_Quarters3", stringsAsFactors = F)
-plot(quarter.shp)
-quarter.shp@data$id <- as.numeric(quarter.shp@data$id)
-quarter_area_temp <- quarter.shp@data
+quarter_shp <- readOGR(dsn = "GIS", layer = "CPH_Quarters3", stringsAsFactors = F)
+plot(quarter_shp)
+quarter_shp@data$id <- as.numeric(quarter_shp@data$id)
+quarter_area_temp <- quarter_shp@data
 quarter_area_temp$area <- NULL
 
 # Find area of all quarters:
 #https://goo.gl/No33KP
-areas <- sapply(slot(quarter.shp, "polygons"), slot, "area")
+areas <- sapply(slot(quarter_shp, "polygons"), slot, "area")
 quarter_area <- cbind(quarter_area_temp, areas)
 rm(areas, quarter_area_temp)
 
@@ -160,3 +168,14 @@ plot(quarter_summary$cum_incidence ~ quarter_summary$pop_density)
 
 
 
+
+# # AGE_ADJUSTED MORTALITY -------------------------------------------------
+# mortality_rates <- age_ts$age[1:22]
+# mortality_rates <- as.data.frame(mortality_rates)
+# mortality_rates$rate <- age_mortality$total_sick / age_ts$total1853[1:22]
+# mortality_rates <- plyr::rename(mortality_rates, replace = c('mortality_rates' = 'age'))
+# 
+# plot(mortality_rates)
+# 
+# save(mortality_rates, file = 'Rdata/mortality_rates.Rdata')
+# 
