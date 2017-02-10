ae64180c8013f251b1f917de527ba16fe438736b
Matthew Phelps <mdp553@hotmail.com>
2017-02-10 09:33:47 +0100

    Update DIC-WAIC work flow

---
+++ multi-neighbor/DIC-Sample.R
@@ -0,0 +1,43 @@
+# Author: Matthew Phelps
+# Desc: DIC sampling for all model runs
+library(runjags)
+library(rjags)
+library(parallel)
+# Function ----------------------------------------------------------------
+
+SampleDIC_rep <- function(x, cl_num){
+  cl <- makeCluster(cl_num)
+  dic_samp <- parLapply(cl, x, extract.runjags, "dic")
+  stopCluster(cl)
+  dic_samp
+}
+
+# RUN FUNCTION --------------------------------------------------------------------
+
+load(file = "Data/Rdata/jags_m1_ls-new-inits.Rdata")
+DIC_m1_ls <- SampleDIC_rep(jags_m1_ls, 6)
+rm(jags_m1_ls)
+# 
+# load(file = "Data/Rdata/jags_m2_ls-new-inits.Rdata")
+# DIC_m2_ls <- SampleDIC_rep(jags_m2_ls, 6)
+# rm(jags_m2_ls)
+
+load(file = "Data/Rdata/jags_m3_ls-new-inits.Rdata")
+DIC_m3_ls <- SampleDIC_rep(jags_m3_ls, 6)
+rm(jags_m3_ls)
+
+load(file = "Data/Rdata/jags_m4_ls-new-inits.Rdata")
+DIC_m4_ls <- SampleDIC_rep(jags_m4_ls, 6)
+rm(jags_m4_ls)
+
+load(file = "Data/Rdata/jags_m5_ls-new-inits.Rdata")
+DIC_m5_ls <- SampleDIC_rep(jags_m5_ls, 6)
+rm(jags_m5_ls)
+
+
+DIC_list <- list(DIC_m1_ls, DIC_m3_ls,
+                  DIC_m4_ls, DIC_m5_ls)
+
+
+# SAVE --------------------------------------------------------------------
+save(DIC_list, file = "Data/Rdata/DIC_ls.Rdata")
