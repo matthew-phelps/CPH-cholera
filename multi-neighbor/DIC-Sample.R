d084b8978d45703914627ac7ac73511b859727ba
Matthew Phelps <mdp553@hotmail.com>
2017-02-15 17:55:13 +0100

    Add model 6 to DIC

--- multi-neighbor/DIC-Sample.R
+++ multi-neighbor/DIC-Sample.R
@@ -35,9 +35,13 @@ load(file = "Data/Rdata/jags_m5_ls.Rdata")
 DIC_m5_ls <- SampleDIC_rep(jags_m5_ls, 6)
 rm(jags_m5_ls)
 
+load(file = "Data/Rdata/jags_m6_ls.Rdata")
+DIC_m6_ls <- SampleDIC_rep(jags_m6_ls, 5)
+rm(jags_m5_ls)
 
 DIC_list <- list(DIC_m1_ls, DIC_m2_ls, DIC_m3_ls,
-                  DIC_m4_ls, DIC_m5_ls)
+                  DIC_m4_ls, DIC_m5_ls, DIC_m6_ls)
+
 
 
 # SAVE --------------------------------------------------------------------
