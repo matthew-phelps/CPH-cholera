f475519e36ac386637d11e884adb313a4a1e3053
Matthew Phelps <mdp553@hotmail.com>
2017-03-02 11:32:08 +0100

    Correct simulation to include phi variations
    Previously only allowed Phi to vary at one part of code

--- multi-neighbor/simulation.R
+++ multi-neighbor/simulation.R
@@ -1,9 +1,12 @@
 # Copenhagen Simulation
 
-simPlusOne <- function(loops, gamma=gamma, I_reps=I_reps, N_it=N_it){
+simPlusOne <- function(loops, I_reps=I_reps, N_it=N_it,
+                       betas_95hpd, phi_95hpd,
+                       gamma_95hpd){
   set.seed(13)
   # browser()
-  gamma <- gamma
+  
+  
   Lambda_est_pe <-  matrix(nrow = Nsteps, ncol = Nquarter)
   LambdaR <-        matrix(nrow = Nsteps, ncol = Nquarter)
   R_new <-          matrix(nrow = Nsteps, ncol = Nquarter)
@@ -28,17 +31,17 @@ simPlusOne <- function(loops, gamma=gamma, I_reps=I_reps, N_it=N_it){
     for (t in 1:(Nsteps-1)){
       # browser()
       for(i in 1:Nquarter){
-        
-        Lambda_est_pe[t, i] <- S_plus1_mat[t, i] / N_it[i] * sum(betas[, i] * I_prev_vect[t, ])
-        LambdaR[t, i] <- I_prev_vect[t, i] * gamma
+        # browser()
+        Lambda_est_pe[t, i] <- S_plus1_mat[t, i] / N_it[i] * sum(betas_95hpd[[z]][, i] * I_prev_vect[t, ])
+        LambdaR[t, i] <- I_prev_vect[t, i] * gamma_95hpd[z, ]
         R_new[t, i] <- min(LambdaR[t, i], I_prev_vect[t, i]) # no more recovereds than infected
         
         I_data <- I_reps[[rand_realization[z]]][t, i] # Observed data
         
-        I_new[t, i] <- rpois(1, (Lambda_est_pe[t, i] * phi ) )
-        I_prev_vect[t + 1, i] <- max(0, (I_prev_vect[t, i] + I_data / phi  - R_new[t, i]))
+        I_new[t, i] <- rpois(1, (Lambda_est_pe[t, i] * phi_95hpd[z, ] ) )
+        I_prev_vect[t + 1, i] <- max(0, (I_prev_vect[t, i] + I_data / phi_95hpd[z, ]  - R_new[t, i]))
         
-        S_temp <- S_plus1_mat[t, i] -    I_data / phi # Should be I_reps instead?
+        S_temp <- S_plus1_mat[t, i] -    I_data /  phi_95hpd[z, ] # Should be I_reps instead?
         S_plus1_mat[t + 1, i] <- max(0, S_temp)
         
       }
