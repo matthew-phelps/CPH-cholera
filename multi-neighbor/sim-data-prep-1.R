8e280469cc88dc051b2071888d818fd10aa0cf32
Matthew Phelps <mdp553@hotmail.com>
2017-03-15 13:59:12 +0100

    Fix model 2 for WAIC
    Forgot to add phi to density calculation

--- multi-neighbor/sim-data-prep-1.R
+++ multi-neighbor/sim-data-prep-1.R
@@ -26,11 +26,14 @@ R_model1 <- rbind(R_list$R_int, R_list$R_ext, R_list$R_tot)
 save(mcmc_out, file = 'Data/Rdata/sim-model-1-data-1.Rdata' )
 save(R_model1, file = 'Data/Rdata/r-values-model-1.Rdata')
 
+
+
+
+
 # MODEL 2 ---------------------------------------------------------------
 load(file = "Data/Rdata/jags_m2_ls-new.Rdata")
 x <- mcmcPrep(jags_m2_ls, q_names, testing = FALSE)
 rm(jags_m2_ls)
-x$int_hpd$int_hpd
 gc()
 mcmc_out <- smMcmc(x)
 rm(x)
@@ -41,6 +44,11 @@ R_list <- RCalc(betas = mcmc_out$betas_95hpd,
 R_model2 <- rbind(R_list$R_int, R_list$R_ext, R_list$R_tot)
 save(mcmc_out, file = 'Data/Rdata/sim-model-2-data-1.Rdata' )
 save(R_model2, file = 'Data/Rdata/r-values-model-2.Rdata')
+
+
+
+
+
 
 
 # MODEL 3 -----------------------------------------------------------------
@@ -61,6 +69,13 @@ R_model3 <- rbind(R_list$R_int, R_list$R_ext, R_list$R_tot)
 save(mcmc_out, file = 'Data/Rdata/sim-model-3-data-1.Rdata' )
 save(R_model3, file = 'Data/Rdata/r-values-model-3.Rdata')
 
+
+
+
+
+
+
+
 # MODEL 4 -----------------------------------------------------------------
 load(file = "Data/Rdata/jags_m4_ls-new.Rdata")
 x <- mcmcPrep(jags_m4_ls, q_names, testing = FALSE)
@@ -77,6 +92,13 @@ R_model4 <- rbind(R_list$R_int, R_list$R_ext, R_list$R_tot)
 
 save(mcmc_out, file = 'Data/Rdata/sim-model-4-data-1.Rdata' )
 save(R_model4, file = 'Data/Rdata/r-values-model-4.Rdata')
+
+
+
+
+
+
+
 
 # MODEL 5 ---------------------------------------------------------------
 load(file = "Data/Rdata/jags_m5_ls-new.Rdata")
