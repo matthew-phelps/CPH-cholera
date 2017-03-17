478c45cbab638f166207dc126255b9067386c497
Matthew Phelps <mdp553@hotmail.com>
2017-03-17 11:42:04 +0100

    Add matrix of median R values as output
    Also clean up function - put some script into helper functions

--- Functions/CalculateRFun.R
+++ Functions/CalculateRFun.R
@@ -1,24 +1,35 @@
 
 RCalc <- function(betas, lo_hpd, hi_hpd, gamma, q_names, 
-                 order=TRUE){
+                  order=TRUE){
   # Find R for each mcmc chain (each combination of beta & gamma)
   # Order = T specifies that quarters are arranged alphabetically
   
-  # Make gamma into list for easy mapply
-  gam_list <- as.list(gamma[,1]) 
-  Rest <- function(x,y) x / y 
-  x <- mapply(Rest, betas, gam_list, SIMPLIFY = FALSE)
-  ## TEST that function is calculating correctly. Evaluates to TRUE
-   stopifnot(all(x[[2]]== betas[[2]] / gamma[2, ]))
- 
-  # Convert earch mcmc iteraction set to one numeric vector
-  z1 <- lapply(x, function(x)  as.vector(as.matrix(x)))
   
-  # bind rows together so each row is mcmc iter
-  z2 <- data.frame(matrix(unlist(z1), nrow = length(z1), byrow = T))
-  R_hi <- sapply(z2, quantile, 0.975)
-  R_lo <- sapply(z2, quantile, 0.025)
-  R_median <- sapply(z2, median)
+  rForEachMcmc <- function(gamma){
+    # R = beta / gamma
+    gam_list <- as.list(gamma[,1]) # Make gamma into list for easy mapply
+    Rest <- function(x,y) x / y 
+    x <- mapply(Rest, betas, gam_list, SIMPLIFY = FALSE)
+    return(x)
+  }
+  x <- rForEachMcmc(gamma)
+  ## TEST that function is calculating correctly. Evaluates to TRUE
+  stopifnot(all(x[[2]]== betas[[2]] / gamma[2, ]))
+  
+  rListToDataFrame <- function(x){
+    # Convert earch mcmc iteraction set to one numeric vector of length = number
+    # of parameters
+    z1 <- lapply(x, function(x)  as.vector(as.matrix(x)))
+    # Bind rows together so each row is mcmc iter
+    z2 <- data.frame(matrix(unlist(z1), nrow = length(z1), byrow = T))
+    return(z2)
+  }
+  
+  r_vals <- rListToDataFrame(x)
+  
+  R_hi <- sapply(r_vals, quantile, 0.975)
+  R_lo <- sapply(r_vals, quantile, 0.025)
+  R_median <- sapply(r_vals, median)
   mkMat <- function(x){
     # browser()
     dimx <- sqrt(length(x))
@@ -30,21 +41,21 @@ RCalc <- function(betas, lo_hpd, hi_hpd, gamma, q_names,
   R_median <- mkMat(R_median)
   
   ## TEST that the rows and columns are in correct order
-  quantile(z2$X2, 0.975) == R_hi[2,1] & quantile(z2$X10, 0.025) == R_lo[1,2]
+  stopifnot(quantile(r_vals$X2, 0.975) == R_hi[2,1] & quantile(r_vals$X10, 0.025) == R_lo[1,2])
   
   ## R - TOTAL
-  ##
-  # browser()
-  R_tot_lo <- rowSums(R_lo, na.rm = T)
-  R_tot_hi <- rowSums(R_hi, na.rm = T)
-  R_tot <- data.frame(rowSums(R_median, na.rm = T))
-  
-  R_tot <- R_tot %>%
-    `colnames<-` ("R_median") %>%
-    mutate(lower = R_tot_lo,
-           upper = R_tot_hi,
-           quarter = q_names,
-           R_type = "tot")
+  rTotal <- function(R_lo, R_hi, R_median){
+    R_tot <- R_median %>%
+      rowSums(na.rm = TRUE) %>%
+      data.frame()%>%
+      `colnames<-` ("R_median") %>%
+      mutate(lower =  rowSums(R_lo, na.rm = T),
+             upper = rowSums(R_hi, na.rm = T),
+             quarter = q_names,
+             R_type = "tot")
+    return(R_tot)
+  }
+  R_tot <- rTotal(R_lo, R_hi, R_median)
   
   
   ## R-INTERNAL
@@ -84,9 +95,18 @@ RCalc <- function(betas, lo_hpd, hi_hpd, gamma, q_names,
     R_ext$quarter <- factor(R_ext$quarter, levels = R_int$quarter[order(R_int$R_median)])
     R_tot$quarter <- factor(R_tot$quarter, levels = R_int$quarter[order(R_int$R_median)])
   }
+  medianValsToDataFrame <- function(R_median){
+    R_median <- R_median %>%
+      data.frame%>%
+      `colnames<-`(q_names) %>%
+      `row.names<-`(q_names)
+  }
   
+  R_median <- medianValsToDataFrame(R_median)
   
-  return(list(R_int = R_int,
+  return(list(R_median = R_median,
+              R_int = R_int,
               R_ext = R_ext,
               R_tot = R_tot))
+  
 }
