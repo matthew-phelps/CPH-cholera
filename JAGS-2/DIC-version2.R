67088f339a30ec6376597a9ab18d239e9750ad57
Matthew Phelps <mdp553@hotmail.com>
2017-04-26 11:48:39 +0200

    DIC and WAIC for model version 2

---
+++ JAGS-2/DIC-version2.R
@@ -0,0 +1,63 @@
+# Author: Matthew Phelps
+# Desc: DIC sampling for all model runs
+library(runjags)
+library(rjags)
+library(parallel)
+
+# Function ----------------------------------------------------------------
+SampleDIC_rep <- function(x, cl_num){
+  # Sample DIC from all epidemic relizations using multiple cores
+  cl <- makeCluster(cl_num)
+  dic_samp <- parLapply(cl, x, extract.runjags, "dic")
+  stopCluster(cl)
+  dic_samp
+}
+
+# RUN FUNCTION --------------------------------------------------------------------
+model_num <- c(1:2)
+
+file_name <- paste("Data/Rdata/model_", model_num[1], "_jags.Rdata", sep="")
+
+x <- lapply(model_num, function(x){
+  file_name <- paste("Data/Rdata/model_", x, "_jags.Rdata", sep="")
+  load(file_name)
+  DIC <- SampleDIC_rep(fit_model, 5)
+  return(DIC)
+})
+
+
+load(file = "Data/Rdata/jags_m1_ls-new-inits.Rdata")
+DIC_m1_ls <- SampleDIC_rep(jags_m1_ls, 6)
+rm(jags_m1_ls)
+
+load(file = "Data/Rdata/jags_m2_ls-new.Rdata")
+DIC_m2_ls <- SampleDIC_rep(jags_m2_ls, 5)
+rm(jags_m2_ls)
+
+load(file = "Data/Rdata/jags_m3_ls-new.Rdata")
+DIC_m3_ls <- SampleDIC_rep(jags_m3_ls, 6)
+rm(jags_m3_ls)
+
+load(file = "Data/Rdata/jags_m4_ls-new.Rdata")
+DIC_m4_ls <- SampleDIC_rep(jags_m4_ls, 4)
+rm(jags_m4_ls)
+
+load(file = "Data/Rdata/jags_m5_ls-new.Rdata")
+DIC_m5_ls <- SampleDIC_rep(jags_m5_ls, 6)
+rm(jags_m5_ls)
+
+load(file = "Data/Rdata/jags_m6_ls-new.Rdata")
+DIC_m6_ls <- SampleDIC_rep(jags_m6_ls, 6)
+rm(jags_m6_ls)
+
+load(file = "Data/Rdata/jags_m7_ls-new.Rdata")
+DIC_m7_ls <- SampleDIC_rep(jags_m7_ls, 4)
+rm(jags_m7_ls)
+
+
+DIC_list <- list(DIC_m1_ls, DIC_m2_ls, DIC_m3_ls,
+                 DIC_m4_ls, DIC_m5_ls, DIC_m6_ls)
+
+
+# SAVE --------------------------------------------------------------------
+save(DIC_list, file = "Data/Rdata/DIC_ls.Rdata")
---
+++ JAGS-2/WAIC-version2.R
@@ -0,0 +1,61 @@
+# Author: Matthew Phelps - using code from Matt Denwood (WAIC function)
+# Desc: WAIC sampling for all model runs
+rm(list=ls())
+library(runjags)
+library(rjags)
+source("Functions/WAIC-function.R")
+
+
+# Function ----------------------------------------------------------------
+
+waicRep <- function(x) {
+  waic_list_tmp <- list()
+  for(i in 1:length(x)){
+    ll <- jags.samples(as.jags(x[[i]]), c('lik', 'llsim'), type=c('mean','variance'), 10000)
+    
+    mean_lik <- apply(ll$mean$lik,c(1,2),mean)
+    
+    var_loglik <- apply(ll$variance$llsim, c(1,2),mean)
+    # Remove first row because we start at t + 1
+    mean_lik <- mean_lik[2:nrow(mean_lik), ]
+    var_loglik <- var_loglik[2:nrow(var_loglik), ]
+    waic_list_tmp[[i]] <-  get_waic(mean_lik, var_loglik)
+  }
+  waic_list_tmp
+}
+
+
+
+# RUN FUNCTION --------------------------------------------------------------------
+
+load(file = "Data/Rdata/model_1_jags.Rdata")
+waic_m1_ls <- waicRep(fit_model)
+rm(jags_m1_ls)
+
+load(file = "Data/Rdata/jags_m2_ls-new.Rdata")
+waic_m2_ls <- waicRep(jags_m2_ls)
+rm(jags_m2_ls)
+
+load(file = "Data/Rdata/jags_m3_ls-new.Rdata")
+waic_m3_ls <- waicRep(jags_m3_ls)
+rm(jags_m3_ls)
+
+load(file = "Data/Rdata/jags_m4_ls-new.Rdata")
+waic_m4_ls <- waicRep(jags_m4_ls)
+rm(jags_m4_ls)
+
+load(file = "Data/Rdata/jags_m5_ls-new.Rdata")
+waic_m5_ls <- waicRep(jags_m5_ls)
+rm(jags_m5_ls)
+
+load(file = "Data/Rdata/jags_m6_ls-new.Rdata")
+waic_m6_ls <- waicRep(jags_m6_ls)
+rm(jags_m6_ls)
+
+waic_list <- list(waic_m1_ls = waic_m1_ls, waic_m2_ls = waic_m2_ls,
+                  waic_m3_ls = waic_m3_ls, waic_m4_ls = waic_m4_ls,
+                  waic_m5_ls = waic_m5_ls, waic_m6_ls = waic_m6_ls)
+
+
+# SAVE --------------------------------------------------------------------
+save(waic_list, file = "Data/Rdata/waic_ls.Rdata")
